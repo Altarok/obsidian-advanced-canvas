852f1f393b25d653374e1f2a82ab087e8e18a61f
Altarok <thom.hammerklinge@gmail.com>
2026-05-14 19:59:30 +0100

    further near original plugn formatting

--- src/canvas-extensions/copy-node-reference-canvas-extension.ts
+++ src/canvas-extensions/copy-node-reference-canvas-extension.ts
@@ -1,20 +1,24 @@
-import {Canvas} from 'src/@types/Canvas'
-import CanvasHelper, {MenuOption} from 'src/utils/canvas-helper'
+import { Canvas } from 'src/@types/Canvas'
+import CanvasHelper, { MenuOption } from 'src/utils/canvas-helper'
 import CanvasExtension from './canvas-extension'
-import {Notice} from 'obsidian'
-import {AdvancedCanvasPluginSettingsValues} from "../settings";
+import { Notice, TFile } from 'obsidian'
+import TextHelper from "../utils/text-helper";
+import { CanvasNodeData } from "obsidian/canvas";
 
 export default class CopyNodeReferenceCanvasExtension extends CanvasExtension {
-
   isEnabled() { return 'enableSingleNodeLinks' as const }
 
   init() {
-    this.plugin.registerEvent(this.plugin.app.workspace.on('advanced-canvas:popup-menu-created',
+    this.plugin.registerEvent(this.plugin.app.workspace.on(
+      'advanced-canvas:popup-menu-created',
       (canvas: Canvas) => this.onPopupMenuCreated(canvas)
     ))
   }
 
   private onPopupMenuCreated(canvas: Canvas): void {
+    const popupMenuEl = canvas?.menu?.menuEl
+    if (!popupMenuEl) return
+
     const selectionNodeData = canvas.getSelectionData().nodes
     if (selectionNodeData.length !== 1) return
 
@@ -22,20 +26,18 @@ export default class CopyNodeReferenceCanvasExtension extends CanvasExtension {
       id: 'node-popup-menu-option-copy-reference',
       label: 'Copy wikilink to node',
       icon: 'link',
-      callback: () => {
-        const selectedNode = selectionNodeData[0]!
-        const ref = `[[${canvas.view.file.path}#${selectedNode.id}]]`
-
-        navigator.clipboard.writeText(ref).then(() => {
-          new Notice('Reference copied to clipboard')
-        }).catch((err) => {
-          new Notice('Failed to copy reference')
-          console.error(err)
-        })
-      }
+      callback: () => CopyNodeReferenceCanvasExtension.copyWikilinkToNode(canvas.view.file, selectionNodeData[0]!)
     }
 
-    CanvasHelper.addPopupMenuOption(canvas, CanvasHelper.createPopupMenuOption(menuOption))
+    const popupMenuOption = CanvasHelper.createPopupMenuOption(menuOption);
+    CanvasHelper.addPopupMenuOption(canvas, popupMenuOption)
+  }
+
+  static copyWikilinkToNode(file: TFile, nodeData: CanvasNodeData){
+    const wikilink = `[[${file.path}#${nodeData.id}|${file.name} (${TextHelper.toTitleCase(nodeData.type)} node)]]`
+    navigator.clipboard.writeText(wikilink).then(() =>
+      new Notice("Copied wikilink to node to clipboard.", 2000)
+    ).catch(() => new Notice("Failed to copy wikilink to node to clipboard.", 2000))
   }
 
 }
