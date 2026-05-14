64dd5bbef2c7c66575e5d54ed44429adca023ea6
Developer-Mike <delta.cephei@outlook.com>
2026-05-14 08:20:26 +0200

    updated lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin"
 import obsidianmd from "eslint-plugin-obsidianmd"
 import { defineConfig, globalIgnores } from "eslint/config"
 import globals from "globals"
@@ -31,32 +30,4 @@ export default defineConfig([
     },
 	},
 	...obsidianmd.configs.recommended,
-	{
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-    },
-    rules: {
-      "no-unused-vars": "off",
-
-      "@typescript-eslint/no-unused-vars": ["error", {
-        args: "none",
-      }],
-
-      "@typescript-eslint/ban-ts-comment": "off",
-      "no-prototype-builtins": "off",
-      "@typescript-eslint/no-empty-function": "off",
-      "@typescript-eslint/no-this-alias": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-floating-promises": "off",
-      "@typescript-eslint/no-base-to-string": "off",
-
-      "@typescript-eslint/no-unsafe-member-access": "off",
-      "@typescript-eslint/no-unsafe-assignment": "off",
-      "@typescript-eslint/no-unsafe-argument": "off",
-      "@typescript-eslint/no-unsafe-return": "off",
-      "@typescript-eslint/no-unsafe-call": "off",
-
-      "obsidianmd/ui/sentence-case": "off",
-    },
-	},
 ])
